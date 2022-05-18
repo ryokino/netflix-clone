27d1b63eabbfe311d4252e35ec82aa357adc99ce
ryokino <ryo.kinoshita212@gmail.com>
2022-05-18 19:04:46 +0900

    day2 complete

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,5 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
+import { useRecoilState } from 'recoil'
+import { modalState, movieState } from '../atoms/modalAtom'
 import { baseUrl } from '../constants/movie'
 import { Movie } from '../typings'
 
@@ -9,8 +11,16 @@ interface Props {
   movie: Movie
 }
 const Thumbnail = ({ movie }: Props) => {
+    const [showModal, setShowModal] = useRecoilState(modalState)
+    const [currentMovie, setCurrentMovie] = useRecoilState(movieState)
   return (
-    <div className="relative h-20 min-w-[100px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105 ">
+    <div
+      className="relative h-20 min-w-[100px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105"
+      onClick={() => {
+        setCurrentMovie(movie)
+        setShowModal(true)
+      }}
+    >
       <Image
         src={`https://image.tmdb.org/t/p/w500${
           movie.backdrop_path || movie.poster_path
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import Head from 'next/head'
+import { useRecoilValue } from 'recoil'
+import { modalState } from '../atoms/modalAtom'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Modal from '../components/Modal'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
 import { Movie } from '../typings'
 import requests from '../utils/request'
 
@@ -26,6 +30,11 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
+  const { loading } = useAuth()
+  const showModal = useRecoilValue(modalState)
+
+  if (loading) return null
+
   return (
     <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
@@ -50,6 +59,7 @@ const Home = ({
         </section>
       </main>
       {/* Modal */}
+      {showModal && <Modal />}
     </div>
   )
 }
